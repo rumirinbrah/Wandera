3f193470c1ed4d70cabebfb20b65265df4735a8f
Atharva Pajgade <atharvapajgade2004@gmail.com>
2025-06-04 10:41:09 +0530

    Confirm delete dialog

---
+++ core/src/main/java/com/zzz/core/presentation/dialogs/ConfirmDeleteDialog.kt
@@ -0,0 +1,87 @@
+package com.zzz.core.presentation.dialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.zzz.core.presentation.components.VerticalSpace
+import com.zzz.core.theme.WanderaTheme
+
+@Composable
+fun ConfirmDeleteDialog(
+    title : String,
+    onDelete:()->Unit,
+    onCancel:()->Unit,
+    modifier: Modifier = Modifier
+)
+{
+    Dialog(
+        onDismissRequest = {
+            onCancel()
+        }
+    ) {
+        Column(
+            modifier
+                .clip(Shapes().large)
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp) ,
+            horizontalAlignment = Alignment.CenterHorizontally ,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            VerticalSpace(5.dp)
+            Text(title , fontWeight = FontWeight.Bold , fontSize = 18.sp)
+            Row (
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ){
+                Button(
+                    modifier = Modifier.weight(1f),
+                    onClick = onCancel,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    )
+                ) {
+                    Text("Cancel",color = MaterialTheme.colorScheme.onPrimaryContainer)
+                }
+                Button(
+                    modifier = Modifier.weight(1f),
+                    onClick = onDelete,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.errorContainer
+                    )
+                ) {
+                    Text("Delete", color = MaterialTheme.colorScheme.onErrorContainer)
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DeletePrev() {
+    WanderaTheme {
+        ConfirmDeleteDialog(
+            title = "Are yo sure u wann delete?",
+            onDelete = {},
+            onCancel = {}
+        )
+    }
+}
