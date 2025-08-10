987a633e23651416ac16b45b221f6ccbe40dec57
Atharva Pajgade <atharvapajgade2004@gmail.com>
2025-08-11 00:38:24 +0530

    Data migration object

---
+++ data/src/main/java/com/zzz/data/db/migrations/WanderaMigrations.kt
@@ -0,0 +1,20 @@
+package com.zzz.data.db.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.zzz.data.util.DbUtils
+
+object WanderaMigrations {
+
+    val MIGRATION_1_2 = object : Migration(1,2){
+        override fun migrate(db: SupportSQLiteDatabase) {
+            db.execSQL(
+                """
+                    ALTER TABLE ${DbUtils.TRACK_EXPENSE_TABLE_NAME} 
+                    ADD COLUMN currencySymbol TEXT NOT NULL DEFAULT '$'
+                """.trimIndent()
+            )
+        }
+    }
+
+}
