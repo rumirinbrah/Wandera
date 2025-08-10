d59544b419da77c7ca2e1daafdf87ec8483e3481
Atharva Pajgade <atharvapajgade2004@gmail.com>
2025-08-11 00:34:42 +0530

    Expense Note Expandable notes

--- feature_trip/src/main/java/com/zzz/feature_trip/overview/presentation/components/ExpensesNoteBox.kt
+++ feature_trip/src/main/java/com/zzz/feature_trip/overview/presentation/components/ExpensesNoteBox.kt
@@ -1,11 +1,7 @@
 package com.zzz.feature_trip.overview.presentation.components
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -15,16 +11,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -38,11 +30,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -55,130 +45,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zzz.core.presentation.buttons.WanderaTextButton
-import com.zzz.core.presentation.components.HazeEffectRectangle
 import com.zzz.core.presentation.components.KeyboardAware
 import com.zzz.core.presentation.components.VerticalSpace
-import com.zzz.core.presentation.components.WanderaShapes
 import com.zzz.core.presentation.modifiers.customShadow
 import com.zzz.core.presentation.modifiers.dynamicHeight
-import com.zzz.core.presentation.modifiers.dynamicWidth
-import com.zzz.core.presentation.text_field.RoundedTextField
 import com.zzz.core.theme.WanderaTheme
 import com.zzz.core.util.shareText
 
-@Composable
-internal fun ExpensesNoteBox(
-    modifier: Modifier = Modifier ,
-    value: String ,
-    onValueChange: (String) -> Unit ,
-    onSave: () -> Unit ,
-    background: Color = MaterialTheme.colorScheme.surfaceContainer ,
-    onBackground: Color = MaterialTheme.colorScheme.onSurface ,
-) {
-    val context = LocalContext.current
-
-
-    var text by remember { mutableStateOf("") }
-
-    Box(
-        modifier
-            .clip(WanderaShapes.singleEdgeShape)
-            .fillMaxWidth()
-            .background(background)
-            .animateContentSize(
-                animationSpec = tween(
-                    800 ,
-                    300
-                )
-            )
-            .padding(
-                start = 4.dp ,
-                end = 4.dp ,
-                top = 4.dp ,
-                bottom = 8.dp
-            )
-    ) {
-        RoundedTextField(
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .align(Alignment.TopStart) ,
-            value = text ,
-            onValueChange = { text = it } ,
-            placeholder = buildAnnotatedString {
-                append("You can save your trip expenses here!\nFor ex, ")
-                withStyle(
-                    SpanStyle(
-                        fontWeight = FontWeight.Bold
-                    )
-                ) {
-                    append("Breakfast : Ryan $25")
-                }
-            } ,
-            placeholderColor = MaterialTheme.colorScheme.onBackground ,
-            shape = WanderaShapes.singleEdgeShape ,
-            background = background ,
-            onBackground = onBackground
-        )
-
-        //action buttons
-        AnimatedVisibility(
-            text.isNotBlank() ,
-            modifier = Modifier.align(Alignment.BottomEnd) ,
-            enter = slideInVertically(
-                animationSpec = tween(500 , 300) ,
-                initialOffsetY = {
-                    it * 2
-                }
-            ) ,
-            exit = slideOutVertically(
-                animationSpec = tween(500 , 300) ,
-                targetOffsetY = {
-                    it * 2
-                }
-            )
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically ,
-                horizontalArrangement = Arrangement.spacedBy(12.dp) ,
-                //modifier = Modifier.align(Alignment.BottomEnd)
-            ) {
-                Box(
-                    Modifier
-                        .clip(CircleShape)
-                        .clickable {
-                            onSave()
-                        }
-                        .padding(4.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(com.zzz.core.R.drawable.save) ,
-                        contentDescription = "Save expense note" ,
-                        modifier = Modifier.size(25.dp) ,
-                        tint = onBackground
-                    )
-                }
-                Box(
-                    Modifier
-                        .clip(CircleShape)
-                        .clickable {
-                            context.shareText(text)
-                        }
-                        .padding(4.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(com.zzz.core.R.drawable.send) ,
-                        contentDescription = "Share expense note" ,
-                        modifier = Modifier.size(25.dp) ,
-                        tint = onBackground
-                    )
-                }
-
-            }
-        }
-
-    }
-
-}
 
 @Composable
 internal fun BookLikeTextField(
@@ -343,10 +216,9 @@ internal fun ExpandableBookLikeTextField(
     onSave: () -> Unit ,
     interactionsEnabled: Boolean = true
 ) {
-    val density = LocalDensity.current
 
     var collapsed by remember { mutableStateOf(true) }
-    var heightActionText = remember(collapsed) {
+    val heightActionText = remember(collapsed) {
         if(collapsed){
             "Show more"
         }else{
