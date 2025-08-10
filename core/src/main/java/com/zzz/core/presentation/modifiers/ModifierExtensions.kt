3ba4caabf6cca5189e8c4254f64b9922ed2cfa3c
Atharva Pajgade <atharvapajgade2004@gmail.com>
2025-08-11 00:37:18 +0530

    Core Bottom sheet updates File manager - txt Haze effect for lazy col Modifier utils

--- core/src/main/java/com/zzz/core/presentation/modifiers/ModifierExtensions.kt
+++ core/src/main/java/com/zzz/core/presentation/modifiers/ModifierExtensions.kt
@@ -1,16 +1,13 @@
 package com.zzz.core.presentation.modifiers
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
